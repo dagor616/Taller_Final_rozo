3952a0f7bb0d590c2a2158b6c8aab7de3059d654
Dylan Perez <xdylanfelipe25@gmail.com>
2026-09-13 17:39:29 -0500

    feat:class de libros con el array creados

--- pt1/src/Crear_Libro.java
+++ pt1/src/Crear_Libro.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Crear_Libro {
-public static void main(String[] args){
+
+    static ArrayList<Libro> libros = new ArrayList<>();
+
+    static HashMap<String, Libro> librosPorISBN = new HashMap<>();
+
+    public static void main(String[] args){
+
+    }
 
 }
+
+// aquí empieza la clase Libro
+class Libro {
+
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private String genero;
+    private int año;
+    private int copias;
 
 }
