87aac1f137716c0aba151c9ceef36fcd90df59bd
Dylan Perez <xdylanfelipe25@gmail.com>
2026-09-13 17:49:26 -0500

    feat:contructor libro hecho

--- pt1/src/Crear_Libro.java
+++ pt1/src/Crear_Libro.java
@@ -22,5 +22,14 @@ class Libro {
     private String genero;
     private int año;
     private int copias;
+//constructor libro
+    public Libro(String titulo, String autor, String isbn, String genero, int año, int copias){
+    this.titulo = titulo;
+    this.autor = autor;
+    this.isbn = isbn;
+    this.genero = genero;
+    this.año = año;
+    this.copias = copias;
+    }
 
 }
