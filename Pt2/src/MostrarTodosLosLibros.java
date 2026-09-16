20ed50eb44bd5cc368f71db245876750d15799e6
Juan Coral <coralpablojua@gmail.com>
2026-09-16 12:20:30 -0500

    En esta segunda parte se nos estaba presentando un error pero sentimos que ya se pudo solucionar

--- Pt2/src/MostrarTodosLosLibros.java
+++ Pt2/src/MostrarTodosLosLibros.java
@@ -1,3 +1,15 @@
-public class MostrarTodosLosLibros {
+import java.util.ArrayList;
 
+public class MostrarTodosLosLibros {
+    private ArrayList<Libro> libros;
+
+    // Constructor
+    public MostrarTodosLosLibros() {
+        libros = new ArrayList<>();
+    }
+
+    // Agregar un libro
+    public void agregarLibro(Libro libro) {
+        libros.add(libro);
+    }
 }
